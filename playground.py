b13e5c63980980852f8a26a9fd120c0895c615ab
jsariol <50191899+jsariol@users.noreply.github.com>
2025-04-19 19:41:11 -0400

    Implement online shopping cart functionality and remove deprecated code

--- playground.py
+++ playground.py
@@ -1,28 +1,9 @@
-item_name = input("Enter food item name:\n")
-item_price = float(input("Enter item price:\n"))
-item_quantity = int(input("Enter item quantity:\n"))
+from collections import namedtuple
 
-print("\nRECEIPT")
-print(f"{item_quantity} {item_name} @ ${item_price:.2f} = ${item_quantity * item_price:.2f}")
-print(f"Total cost: ${item_quantity * item_price:.2f}")
+Player = namedtuple('Player', ['name', 'number', 'position', 'team'])
 
-print()  # <-- línea en blanco
+cam = Player('Cam Newton', '1', 'Quarterback', 'Carolina Panthers')
+lebron = Player('Lebron James', '23', 'Small forward', 'Los Angeles Lakers')
 
-item2_name = input("Enter second food item name:\n")
-item2_price = float(input("Enter item price:\n"))
-item2_quantity = int(input("Enter item quantity:\n"))
-
-print("\nRECEIPT")
-print(f"{item_quantity} {item_name} @ ${item_price:.2f} = ${item_quantity * item_price:.2f}")
-print(f"{item2_quantity} {item2_name} @ ${item2_price:.2f} = ${item2_quantity * item2_price:.2f}")
-
-total_cost = (item_quantity * item_price) + (item2_quantity * item2_price)
-gratuity = total_cost * 0.15
-total_with_tip = total_cost + gratuity
-
-print(f"Total cost: ${total_cost:.2f}")
-
-print()  # <-- línea en blanco
-
-print(f"15% gratuity: ${gratuity:.2f}")
-print(f"Total with tip: ${total_with_tip:.2f}")
+print(cam.name + '(#' + cam.number + ')' + ' is a ' + cam.position + ' for the ' + cam.team + '.')
+print(lebron.name + '(#' + lebron.number + ')' + ' is a ' + lebron.position + ' for the ' + lebron.team + '.')
